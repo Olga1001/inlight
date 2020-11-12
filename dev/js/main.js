b55e8dcc8dbc17d9bf6df526a8f5db072722ac47
Olga <zhuravel.olga95@gmail.com>
2020-11-13 00:58:29 +0200

    in process

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
 
-  // $('.fullpage').fullpage({
-  //     licenseKey: 'YOUR_KEY_HERE',
-  //     navigation: false,
-  //     scrollOverflow: true,
-  //     autoScrolling: true,
-  //     scrollHorizontally: true,
-  //     verticalCentered: true,
-  //     // scrollBar: false,
-  //     // navigationPosition: 'right',
-  //     // normalScrollElements: ".header, .header-search-flex, .header-search-contanet"
-  // });
+  $('.fullpage').fullpage({
+      licenseKey: 'YOUR_KEY_HERE',
+      navigation: false,
+      scrollOverflow: true,
+      autoScrolling: true,
+      scrollHorizontally: true,
+      verticalCentered: true,
+      // scrollBar: false,
+      // navigationPosition: 'right',
+      // normalScrollElements: ".header, .header-search-flex, .header-search-contanet"
+  });
 
   $(".slider").slick({
     slidesToShow: 5,
