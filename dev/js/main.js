08770d2073373d02a32d02d05ec7959d289e16e0
Olga <zhuravel.olga95@gmail.com>
2020-12-03 13:18:48 +0200

    in process

--- dev/js/main.js
+++ dev/js/main.js
@@ -59,12 +59,11 @@ $(document).ready(function () {
     $(".slider").slick(option, {
       responsive: [
         {
-          breakpoint: 561,
+          breakpoint: 562,
           settings: {
             slidesToShow: 3,
             slidesToScroll: 1,
           }
-        
         },
       ]
     });
@@ -123,7 +122,7 @@ $(document).ready(function () {
   // scroll on mousewhele
   $(".section").on("mousewheel", function(event) {
     let _this = $(this);
-   
+    
     _this.onscroll = checkReading;
     if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0 ) {
       _this.removeClass('active');
@@ -136,15 +135,53 @@ $(document).ready(function () {
 
    // scroll on swipe
   $(".section").swipe({
-      swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
-          if (direction == 'up'){
-            $(this).next().addClass('active');
-          } else if (direction == 'down'){
-            $(this).removeClass('active');
-          }
+      swipe:function(event, direction, distance, phase, duration, fingerCount, fingerData) {
+        if (direction == 'up'){
+          $(this).next().addClass('active');
+        } else if (direction == 'down'){
+          $(this).removeClass('active');
+        }
       },
-      allowPageScroll: "auto",
+      allowPageScroll: "vertical",
   });
+
+
+//   function swipeScroll(c,b,pd,f){
+//     $(c).swipe({//c style must be position:absolute;top:0; b style must be position:relative;
+//       swipeStatus:function(event, phase, direction, distance, fingerCount) {
+//         if(phase=="start"){pd=0;}
+//         // if(phase=="end"){return false;}
+//         if(phase=="move" && pd!=distance){
+//           var t = parseInt($(c).css("top"),10);
+//           var u = (pd-distance);
+//           if(direction=="up" && t != u){ t+=u; }else if(direction=="down" && t != -u){ t-=u; }
+//           pd=distance;
+//           if(t > 0 || $(b).height() > $(c).height()){t=0;}//top of content
+//           else if(($(b).height()-t) >= $(c).height()){//bottom of content
+//             t=$(b).height()-$(c).height();
+//             if(typeof f != "undefined"){f();}
+//           }
+//           $(c).css("top",t);
+//         }
+//       }
+//     });
+// }
+
+  // $(".swipe-read").swipe({
+  //   swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+  //     let thisItem =  $(this);
+  //     let maxscroll = thisItem[0].scrollWidth;
+  //     if (direction == 'left'){
+  //       thisItem.animate({
+  //         scrollLeft: -1 * maxscroll
+  //       }, 500);
+  //     } else if (direction == 'right'){
+  //       thisItem.animate({
+  //         scrollLeft: maxscroll
+  //       }, 500);
+  //     }
+  //   },
+  // });
  
   // initialization animation - wow
   new WOW().init();
@@ -154,4 +191,19 @@ $(document).ready(function () {
     $(this).toggleClass('active');
     $(".navtop").toggleClass('active');
   });
+
+function is_mobile() {return (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));};
+
+function fix100vh() {
+  var winHeight = $(window).height();
+  if ( is_mobile() ) {
+    $('.section').css('min-height', winHeight);
+  } 
+}; 
+
+fix100vh();
+
 });
+
+
+
--- dist/js/main.js
+++ dist/js/main.js
@@ -51,7 +51,7 @@ $(document).ready(function () {
   } else {
     $(".slider").slick(option, {
       responsive: [{
-        breakpoint: 561,
+        breakpoint: 562,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1
@@ -125,15 +125,50 @@ $(document).ready(function () {
   }); // scroll on swipe
 
   $(".section").swipe({
-    swipe: function swipe(event, direction, distance, duration, fingerCount, fingerData) {
+    swipe: function swipe(event, direction, distance, phase, duration, fingerCount, fingerData) {
       if (direction == 'up') {
         $(this).next().addClass('active');
       } else if (direction == 'down') {
         $(this).removeClass('active');
       }
     },
-    allowPageScroll: "auto"
-  }); // initialization animation - wow
+    allowPageScroll: "vertical"
+  }); //   function swipeScroll(c,b,pd,f){
+  //     $(c).swipe({//c style must be position:absolute;top:0; b style must be position:relative;
+  //       swipeStatus:function(event, phase, direction, distance, fingerCount) {
+  //         if(phase=="start"){pd=0;}
+  //         // if(phase=="end"){return false;}
+  //         if(phase=="move" && pd!=distance){
+  //           var t = parseInt($(c).css("top"),10);
+  //           var u = (pd-distance);
+  //           if(direction=="up" && t != u){ t+=u; }else if(direction=="down" && t != -u){ t-=u; }
+  //           pd=distance;
+  //           if(t > 0 || $(b).height() > $(c).height()){t=0;}//top of content
+  //           else if(($(b).height()-t) >= $(c).height()){//bottom of content
+  //             t=$(b).height()-$(c).height();
+  //             if(typeof f != "undefined"){f();}
+  //           }
+  //           $(c).css("top",t);
+  //         }
+  //       }
+  //     });
+  // }
+  // $(".swipe-read").swipe({
+  //   swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+  //     let thisItem =  $(this);
+  //     let maxscroll = thisItem[0].scrollWidth;
+  //     if (direction == 'left'){
+  //       thisItem.animate({
+  //         scrollLeft: -1 * maxscroll
+  //       }, 500);
+  //     } else if (direction == 'right'){
+  //       thisItem.animate({
+  //         scrollLeft: maxscroll
+  //       }, 500);
+  //     }
+  //   },
+  // });
+  // initialization animation - wow
 
   new WOW().init(); // burger 
 
@@ -141,4 +176,21 @@ $(document).ready(function () {
     $(this).toggleClass('active');
     $(".navtop").toggleClass('active');
   });
+
+  function is_mobile() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  }
+
+  ;
+
+  function fix100vh() {
+    var winHeight = $(window).height();
+
+    if (is_mobile()) {
+      $('.section').css('min-height', winHeight);
+    }
+  }
+
+  ;
+  fix100vh();
 });
